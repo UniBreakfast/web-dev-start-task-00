b0479b4fb83dc4337e8212a244578749386fdfd7
UniBreakfast <unibreakfast1@gmail.com>
2023-12-21 14:36:54 +0200

    Add checks for Google Chrome, Node.js, Git, and GitHub account

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 const { exec } = require('child_process');
+const https = require('https');
+const fs = require('fs');
 
-// Function to execute a command and print the result
+// Function to execute a command
 function executeCommand(command, successMessage, errorMessage) {
-  exec(command, (error, stdout, stderr) => {
+  exec(command, (error, stdout) => {
     if (error) {
-      console.error(`Error: ${errorMessage}`);
-      console.error(stderr);
+      console.error(errorMessage);
       return;
     }
-    console.log(
-      successMessage,
-      stdout.replace(/\n/g, '').replace(/[a-f0-9]{20}/g, '')
-    );
+    console.log(`${successMessage} ${stdout.trim()}`);
   });
 }
 
-// Check if Git is installed
-executeCommand(
-  'git --version',
-  'Git is installed:',
-  'Git is not installed. Please install Git.'
-);
+// Check for Google Chrome
+const chromePaths = [
+  `${process.env.ProgramFiles}\\Google\\Chrome\\Application\\chrome.exe`,
+  `${process.env['ProgramFiles(x86)']}\\Google\\Chrome\\Application\\chrome.exe`
+];
+const chromeInstalled = chromePaths.some(path => fs.existsSync(path));
 
-// Check if VSCode is installed
-executeCommand(
-  'code --version',
-  'VSCode is installed:',
-  'VSCode is not installed. Please install VSCode.'
-);
+console.log(chromeInstalled ? '✔️  Google Chrome is installed.' : '❌ Google Chrome is not installed.');
+
+// Check Node.js
+executeCommand('node --version', '✔️  Node.js version:', '❌ Node.js is not installed.');
+
+// Check Git
+executeCommand('git --version', '✔️  Git version:', '❌ Git is not installed.');
+
+// Check GitHub Account
+const username = process.argv[2];
+if (!username) {
+  console.error('Please provide a GitHub username.');
+} else {
+  https.get(`https://api.github.com/users/${username}`, { headers: { 'User-Agent': 'Node.js' } }, (res) => {
+    if (res.statusCode === 200) {
+      console.log(`✔️ GitHub account found for username: ${username}`);
+    } else {
+      console.error(`❌ No GitHub account found for username: ${username}`);
+    }
+  }).on('error', (e) => {
+    console.error(`Error: ${e.message}`);
+  });
+}
